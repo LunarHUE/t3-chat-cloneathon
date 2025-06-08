2683ba25354bc5cf78262c43e038355434a3a170
Tristen Dillon <tdill@lunarhue.com>
2025-06-08 21:50:27 +0000

    Lowkey did some more stuff then css but its ok

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,15 @@ import {
   createRouteMatcher,
   nextjsMiddlewareRedirect,
 } from "@convex-dev/auth/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isSignInPage = createRouteMatcher(["/signin"]);
 const isProtectedRoute = createRouteMatcher(["/", "/server"]);
 
 export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
+  // Override middleware until we have a proper auth system
+  return NextResponse.next();
+
   if (isSignInPage(request) && (await convexAuth.isAuthenticated())) {
     return nextjsMiddlewareRedirect(request, "/");
   }
